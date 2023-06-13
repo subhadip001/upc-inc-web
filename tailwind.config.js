f6e2bbaa58202e16f69e6ba209027d17b70d5de7
Atharav <atharvakvrdso@gmail.com>
2023-06-13 17:50:59 +0530

    home section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,21 @@ export default {
     extend: {},
     fontFamily: {
       poppins: "Poppins",
+      lato: "Lato",
     },
     colors: {
       blue: {
         100: "#0044CB",
         200: "#195ABD",
+        300: "#233568",
       },
       black: "#000000",
       white: "#FFFFFF",
       aliceblue: "#F8FDFF00",
+      bglight:
+        "background: radial-gradient(50% 50% at 50% 50%, rgba(248, 253, 255, 0) 0%, #F8FDFF 99.69%)",
+      bisque: "#FFF4CA",
+      grey: "#767C7B",
     },
   },
   plugins: [],
